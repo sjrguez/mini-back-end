429b99a4c932b06b081f740af16153f4b1d67653
Starlin de la cruz <starlinr82@gmail.com>
2019-03-15 21:42:04 -0400

    Cambios en el modelo de usuairo

--- Modelos/usuario.ts
+++ Modelos/usuario.ts
@@ -6,7 +6,7 @@ var UsuarioSchema = Schema({
     nombre:{type:String,required:[true,'EL nombre es requerido']},
     nick:{type:String,required:[true,'El nick es requerido']},
     password:{type:String,required:[true,'El password es requerido']},
-    tipo:{type:String,required:[true,'el tipo de usuario es requerido']},
+    tipo:{ type:Schema.Types.ObjectId, ref:'TipoUsuario',required:[true,'el tipo de usuario es requerido']},
     direccion:{type:String,required:false},
     telefono:{type:String,required:false},
     token:{type:Number,required:false},
